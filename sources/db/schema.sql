950a1a207e0593ff47d72080b887b91f08814b7a
Ram Sah <mohan2036@gmail.com>
2020-08-16 02:43:05 -0400

    update db tables and install dependencies

--- sources/db/schema.sql
+++ sources/db/schema.sql
@@ -7,25 +7,25 @@ USE employees_db;
 -- Creates the table "department" within employee --
 CREATE TABLE department (
   -- Creates a numeric column called "id" automatically increment value as we create new rows --
-  id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL   
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 -- Creates the table "role" within employee --
 CREATE TABLE role (
-  id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
-  department_id INT UNSIGNED NOT NULL
-
+  id INTEGER AUTO_INCREMENT  NOT null PRIMARY KEY,
+  role_title VARCHAR(30) NOT NULL,
+  salary DECIMAL (10,2) NOT NULL,
+  department_id INT
+  -- foreign key(department_id) references department(id) ON DELETE CASCADE
 
 );
 -- Creates the table "employee" within employee --
 CREATE TABLE employee (
-  id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT UNSIGNED NOT NULL,
-  manager_id INT UNSIGNED
-
-  
+  role_id INT NOT NULL,
+  manager_id INT default null
+  -- foreign key(role_id) references role(id) ON DELETE CASCADE 
+  -- foreign key(manager_id) references employee(id) ON DELETE CASCADE
 );
--- sources/db/seed.sql
+++ sources/db/seed.sql
@@ -1,27 +1,32 @@
-INSERT INTO department
-  (name)
+-- insert values for department table
+INSERT INTO department(name)
 VALUES
   ('Sales'),
   ('Engineering'),
   ('Finance'),
-  ('Legal');
-INSERT INTO role
-  (title, salary, department_id)
+  ('Legal'),
+  ("Delivery");
+  -- insert values for role table
+INSERT INTO role(role_title, salary, department_id)
 VALUES
-  ('Sales Lead', 65000 , 1),
-  ('Salesperson', 55000 , 1),
-  ('Lead Engineer', 105000, 2),
-  ('Software Enginner', 85000, 2),
+  ('"Manager"', 110000 , 1),
+  ('Salesperson', 55000 , 3),
+  ('Lead Engineer', 102000, 2),
+  ('Software Enginner', 85000, 1),
   ('Accountant', 105000, 3),
-  ('Legal Team Lead', 125000, 4),
-  ('Lawyer', 105000, 4);
-INSERT INTO employee
-  (first_name, last_name, role_id, manager_id)
+  ('Legal Team Lead', 100000, 4),
+  ('Lawyer', 105000, 5);
+  -- insert values for employee table
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-  ('Jay', 'Jam', 1, NULL),
-  ('Tam', 'Bam', 2, 1),
-  ('John', 'Lam', 3, NULL),
-  ('Dave', 'kave', 4, 3),
-  ('Kim', 'kery', 5, NULL),
-  ('Nim', 'Nam', 6, NULL),
-  ('Zin', 'Vam', 7, 6);
+  ('Kevin', 'varga', 7, 1),
+  ('Mohan', 'Sah', 1, 3),
+  ('Tam', 'Bam', 2, NULL),
+  ('John', 'varga', 3, NULL),
+  ('Dave', 'Doe', 4, 2),
+  ('Jim', 'kuma', 5, NULL),
+  ('piter', 'Duma', 6, NULL);   
+
+Select * from department;
+Select * from role;
+Select * from employee;
